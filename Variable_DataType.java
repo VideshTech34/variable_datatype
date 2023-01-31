7ddc8d4dbb5a411b32bb885869710d6fc029018b
VideshTech34 <videsh5655@gmail.com>
2023-01-31 09:11:03 +0530

    commit first

---
+++ Variable_DataType.java
@@ -0,0 +1,68 @@
+//java Variables and Datatype Assignments
+
+ Q1.What is statically typed and Dynamically typed programming Language?
+Statically typed: Programming languages are referred to as "Static typed" if the
+memory of the variable is provided at the time of compilation.
+For instance, C, C++, and Java
+
+Dynamically typed: Programming languages are said to as "dynamically typed"
+if the memory for the variable is provided at the time of execution.
+Like Python, PHP, and JavaScript
+
+Q2.What is variable in Java?
+Ans->
+A variable is the name of a memory-allocated reserved area. It may be thought of as
+the name of a memory location, in other words.
+
+* While the Java programme is running, the value is held in a container.
+
+* To identify the storage location, each variable needs to have a special name.
+
+* A data type is assigned to a variable (we will learn about it after this topic).
+
+Q3. How to assign a value to a variable?
+Ans->
+Variable names should not begin with a number.For example
+int 2 var; // 2 var is an invalid variable.
+
+White Spaces are not permitted in variable names. For example,
+int cricket score; // invalid variables.
+There is a gap/whitespace between cricket and score.
+
+A java keyword (reserved word) cannot be used as a variable name. For
+example, int float; is an invalid expression as float is a predefined
+keyword(we will learn about them) in java.
+
+Q4. What are primitive DataType in java?
+Ans->
+* Following are the primitive datatype in java
+ *Boolean data type
+ *byte data type
+ *int data type
+ *long data type
+ *float data type
+ *double data type
+ *char data type
+ *short data type
+
+Q5. What are Identifiers in java?
+Ans->
+An identifier is a name given to a package, class, interface, method, or variable. All identifiers
+must have different names.
+
+Q6. List the operators in java?
+Ans-> Operators can be identified in java
+Operators in Java can be classified into 6 types:
+1. Arithmetic Operators
+2. Relational Operators
+3. Logical Operators
+4. Assignment Operators
+5. Unary Operators
+6. Bitwise Operators
+
+Q7. Explain about increment and decrement operator in java?
+Ans->
+these are techniques which we use at the time of increment and decrement operator
+Increment and decrement operators are unary operators. We can only apply these operators on a single operand, hence these operators are called as unary operators.
+
+we can apply these unary operators on all primitive types except Boolean.
